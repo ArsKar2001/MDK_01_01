1f06c41c90831f7a366c8224540eb463ffc278a1
arseniy <arseniy.karmanov@mail.ru>
2020-09-20 14:10:09 +0900

    Add jar

--- src/launching/Main.java
+++ src/launching/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../display/MainScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/display/MainScene.fxml"));
         primaryStage.setTitle("МДК 01.01 Разработка программных модулей");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
